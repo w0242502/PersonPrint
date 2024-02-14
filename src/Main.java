dce501ec43b4e6869cc89b3c36a8db8f9898c9bf
David Kristiansen <David.Kristiansen@nscc.ca>
2024-02-14 12:42:23 -0400

    Added basic code to print person name and age.

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+/**
+ * Main.java
+ * Author: David Kristiansen (W0242502)
+ * Date: 2024-02-14
+ * Time: 10:35 a.m.
+ * Description:
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Prompt user for their name and age
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String name = scanner.nextLine();
+
+
+        int age=-1;
+        while (age == -1) {
+            try {
+                System.out.println("Please enter your age:");
+                age = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                age = -1;
+                System.out.println("Error! Please enter a valid number for age.");
+                scanner.next();
+            }
+        }
+
+        printPerson(name, age);
+    }
+
+    public static void printPerson(String name, int age) {
+        System.out.println("Hi "+ name + "! You are "+ age + " years young!");
+    }
+}
